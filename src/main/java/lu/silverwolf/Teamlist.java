8cfae1359cd8df7200a4a200a230415982e91c2e
Mr-LegitHD <cicgi526@school.lu>
2021-02-06 16:45:18 +0100

    little fix
    little fix

--- src/main/java/lu/silverwolf/Teamlist.java
+++ src/main/java/lu/silverwolf/Teamlist.java
@@ -32,8 +32,8 @@ public class Teamlist extends ListenerAdapter
            /* Owner */ info.addField("Owner","<@750768307333890178>", false);
            /* Admin */ info.addField("Admin","<@!747514278071894086>", false);
            /* Event-Manager */ info.addField("Event-Manager","<@!483957638212747264>", false);
-           /* Head-Mod */ info.addField("Head-Mod","<@745273256973566059>", false);
-           /* Mod */ info.addField("Mod","<@745273256973566059>\n<@!483957638212747264>\n<@505024573448912898>\n<@343044879418392576>", false);
+           /* Head-Mod */ info.addField("Head-Mod","<@524581298875138098>", false);
+           /* Mod */ info.addField("Mod","<@745273256973566059>\n<@!483957638212747264>\n<@505024573448912898>\n<@343044879418392576>\n<@524581298875138098>", false);
            /* Bot-Developer */ info.addField("Bot-Developer","<@722854351600615465>", false);
             info.setFooter("System");
             info.setThumbnail("https://i.imgur.com/ek2VZ07.png");
